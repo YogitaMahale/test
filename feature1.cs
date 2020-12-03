2f2ed92d62c7ca0a0a26e8be892bca7a41daeab8
Yogtiamahalee <mahaleyogita233@gmail.com>
2020-12-03 23:08:20 +0530

    add function

--- feature1.cs
+++ feature1.cs
@@ -7,5 +7,12 @@ namespace WebApplication1
 {
     public class feature1
     {
+        public int add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int res = x1 + x2;
+            return res;
+        }
     }
 }
